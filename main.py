de7523ce7124683a6242d3162b146e0610b04a3d
Alex-Matveenko <matveenko.a.p@gmail.com>
2022-09-03 20:23:22 +0300

    Four commit
    add print and move if-exit into else loop

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+# Игра в слова
 name_list = []
 word_list = []
+print('Вас приветсвует игра в слова.')
+print('Для начала введите имя игрока. Введите "start" для начала игры.')
 
 
 def player_name():
@@ -18,22 +21,22 @@ def word():
                 slovo = input(f'{i}, введите слово: \n').lower()
                 word_list.append(slovo)
             else:
+                if slovo == 'stop':
+                    exit()
                 last_word = str(word_list[-1])
                 slovo = input(f'{i}, введите слово на букву "{last_word[-1]}": \n').lower()
-                while slovo.startswith(last_word[-1]) == False:
+                while not slovo.startswith(last_word[-1]):
                     print('Это слово начинаеться с другой буквы!')
                     slovo = input(f'{i}, введите слово на букву "{last_word[-1]}": \n').lower()
                 else:
-
                     while slovo in word_list:
                         print('Это слово уже использовалось, введите другое: \n')
                         slovo = input(f'{i}, введите слово на букву "{last_word[-1]}": \n').lower()
                     else:
                         word_list.append(slovo)
-            if slovo == 'stop':
-                exit()
 
 
 player_name()
+print('Игра началась! Чтобы закончить игру введите "stop".')
 word()
 print(name_list, word_list)

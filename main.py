b9d67503a44c96bf6d45422d4790ecbca77069d0
Alex-Matveenko <matveenko.a.p@gmail.com>
2022-09-03 22:02:17 +0300

    Fifth commit
    fix bag with double mistake

--- main.py
+++ main.py
@@ -21,20 +21,21 @@ def word():
                 slovo = input(f'{i}, введите слово: \n').lower()
                 word_list.append(slovo)
             else:
-                if slovo == 'stop':
-                    exit()
                 last_word = str(word_list[-1])
                 slovo = input(f'{i}, введите слово на букву "{last_word[-1]}": \n').lower()
-                while not slovo.startswith(last_word[-1]):
-                    print('Это слово начинаеться с другой буквы!')
-                    slovo = input(f'{i}, введите слово на букву "{last_word[-1]}": \n').lower()
+                if slovo == 'stop':
+                    exit()
                 else:
-                    while slovo in word_list:
-                        print('Это слово уже использовалось, введите другое: \n')
-                        slovo = input(f'{i}, введите слово на букву "{last_word[-1]}": \n').lower()
-                    else:
-                        word_list.append(slovo)
-
+                    while True:
+                        if not slovo.startswith(last_word[-1]):
+                            print('Это слово начинаеться с другой буквы!')
+                            slovo = input(f'{i}, введите слово на букву "{last_word[-1]}": \n').lower()
+                        elif slovo in word_list:
+                            print('Это слово уже использовалось, введите другое: \n')
+                            slovo = input(f'{i}, введите слово на букву "{last_word[-1]}": \n').lower()
+                        else:
+                            word_list.append(slovo)
+                            break
 
 player_name()
 print('Игра началась! Чтобы закончить игру введите "stop".')

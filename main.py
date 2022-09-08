4158b494e858e1f150f95224351e3b68c742d789
Alex-Matveenko <matveenko.a.p@gmail.com>
2022-09-08 19:02:41 +0300

    add scraping online dictionary, but it was bad idea...
    first it is so long to wait while all dictionary download
    
    Second this dictionary not full and some words are missing
    
    Third main function became very scary))))
    
    keep thinking

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 # Игра в слова
+import requests
+from bs4 import BeautifulSoup as Bs
+from fake_useragent import UserAgent
+
+ua = UserAgent()
+headers = {"User-Agent": ua.random}
+
+
+def rus_dict_parse():
+    page_number = 1
+    for letter in "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯ":
+        url = f"https://ozhegov.slovaronline.com/articles/{letter}/page-{page_number}"
+        response = requests.get(url, headers=headers)
+        soup = Bs(response.text, "lxml")
+        data = soup.find_all("div", class_="col-lg-4 col-md-6 col-sm-12 article-link")
+        if len(data) != 0:
+            page_number += 1
+        else:
+            page_number = 1
+            continue
+        if len(data) != 0:
+            for words in data:
+                dict_word = words.find("a").text.strip(".?").lower()
+                dictionary.append(dict_word)
+        else:
+            break
+
+
+print("Начните игру после появления приветствия на экране")
 name_list = []
 word_list = []
 res = {}
+dictionary = []
+rus_dict_parse()
 print('Вас приветсвует игра в слова.')
 print('Для начала введите имя игрока. Введите "start" для начала игры.')
 
@@ -20,11 +51,23 @@ def word():
         for i in name_list:
             if len(word_list) == 0:
                 slovo = input(f'{i}, введите слово: \n').lower()
+                while slovo not in dictionary:
+                    print("Такого слова не существует!")
+                    slovo = input(f'{i}, введите слово: \n').lower()
                 word_list.append(slovo)
                 res[i] += 1
             else:
                 last_word = str(word_list[-1])
                 slovo = input(f'{i}, введите слово на букву "{last_word[-1]}": \n').lower()
+                while slovo not in dictionary:
+                    print("Такого слова не существует!")
+                    slovo = input(f'{i}, введите слово на букву "{last_word[-1]}": \n').lower()
+                    if slovo == 'stop':
+                        for key, value in res.items():
+                            print(f'{key}, ввел {value} слов!')
+                        print('***********************')
+                        print(f'Всего введено слов: {len(word_list)}')
+                        exit()
                 if slovo == 'stop':
                     for key, value in res.items():
                         print(f'{key}, ввел {value} слов!')
@@ -44,6 +87,9 @@ def word():
                             slovo = input(f'{i}, введите слово на букву "{last_word[-1]}": \n').lower()
                         elif slovo in word_list:
                             print('Это слово уже использовалось, введите другое: \n')
+                            slovo = input(f'{i}, введите слово на букву "{last_word[-1]}": \n').lower()
+                        elif slovo not in dictionary:
+                            print("Такого слова не существует!")
                             slovo = input(f'{i}, введите слово на букву "{last_word[-1]}": \n').lower()
                         else:
                             word_list.append(slovo)
